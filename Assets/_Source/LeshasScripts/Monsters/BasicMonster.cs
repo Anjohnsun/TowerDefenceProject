355bda52764e0211854683795f80abe03c842e4b
ikvelibrium <stalker.alex.perel@mail.ru>
2022-11-30 20:26:59 +0300

    Допиливаю логику волн

--- Assets/_Source/LeshasScripts/Monsters/BasicMonster.cs
+++ Assets/_Source/LeshasScripts/Monsters/BasicMonster.cs
@@ -6,12 +6,38 @@ public class BasicMonster : MonoBehaviour, IMonster
 {
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private Vector3 _trarget;
+    [SerializeField] private GameObject player;
+    [SerializeField] private int _hp;
+    [SerializeField] private int _dealDamage;
+    
     private void Start()
     {
         _agent.SetDestination(_trarget);
     }
     void refreshTarget(Vector3 target)
     {
+       /* if()
+        {
+            _agent.SetDestination(player.transform.position);
+        }
+         if()
+        {
+            _agent.SetDestination(_trarget);
+        } */
+    }
 
+    void getDamage(int damage)
+    {
+
+
+    }
+    void deathChecker()
+    {
+        if (_hp <= 0)
+        {
+            WaveController.DEATHS++;
+            Destroy(gameObject);
+
+        }
     }
 }
--- Assets/_Source/LeshasScripts/Wave/WaveController.cs
+++ Assets/_Source/LeshasScripts/Wave/WaveController.cs
@@ -6,30 +6,70 @@ using UnityEngine;
 public class WaveController : MonoBehaviour
 {
     [SerializeField] private Dictionary<int, string> _enemys = new Dictionary<int, string>();
+    private List<GameObject> _spawnedEnemys = new List<GameObject>();
     [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private GameObject _waveOption;
     [SerializeField] private Vector3 _enemySpawn;
     [SerializeField] private WaveSO FirstWaveSO;
+    [SerializeField] private WaveSO SecondWaveSO;
+    [SerializeField] private WaveSO ThirdWaveSO;
+    public static int DEATHS;
+    private int _waveNumber = 1;
     void Start()
     {
        // _enemys.Add(FirstWaveSO._agressiveAmount, FirstWaveSO._agressiveName);
         _enemys.Add(FirstWaveSO._basicAmount , FirstWaveSO._basicName );
         // _enemys.Add(FirstWaveSO._flyAmount , FirstWaveSO._flyName );
         // _enemys.Add(FirstWaveSO._shootAmount , FirstWaveSO._shootName );
-        for (int i = 0; i < _enemys.Count; i++)
-        {
-            for (int j = 0; j < FirstWaveSO._basicAmount; j++)
-            {
-                Instantiate(_enemyPrefab, _enemySpawn, Quaternion.identity);
-            }
 
+        switch (_waveNumber)
+        {
+            case 1 :
+                for (int i = 0; i < _enemys.Count; i++)
+                {
+                    for (int j = 0; j < FirstWaveSO._basicAmount; j++)
+                    {
+                        Instantiate(_enemyPrefab, _enemySpawn, Quaternion.identity);
+                    }
+                    
+                }
+                
+                break;
+            case 2 :
+                for (int i = 0; i < _enemys.Count; i++)
+                {
+                    for (int j = 0; j < SecondWaveSO._basicAmount; j++)
+                    {
+                        Instantiate(_enemyPrefab, _enemySpawn, Quaternion.identity);
+                    }
+
+                }
+                
+                break;
+            case 3:
+                for (int i = 0; i < _enemys.Count; i++)
+                {
+                    for (int j = 0; j < ThirdWaveSO._basicAmount; j++)
+                    {
+                        Instantiate(_enemyPrefab, _enemySpawn, Quaternion.identity);
+                    }
+
+                }
+                break;
         }
 
+       
     }
-
-    
-    void Update()
+    private void Update()
     {
-        
+        if(DEATHS >= FirstWaveSO._basicAmount && DEATHS < SecondWaveSO._basicAmount)
+        {
+            _waveNumber++;
+            DEATHS = 0;
+        } else if (DEATHS >= SecondWaveSO._basicAmount && DEATHS < ThirdWaveSO._basicAmount)
+        {
+            _waveNumber++;
+            DEATHS = 0;
+        } 
     }
 }
