7c6b8455ef720b73deab2420c6561d3e91e2fc09
ikvelibrium <stalker.alex.perel@mail.ru>
2022-12-01 18:46:37 +0300

    Улучшена система спавна, частично реализована логика монстров

---
+++ Assets/_Source/LeshasScripts/Gates.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gates : IDamagable
+{
+    [SerializeField] private int _hp;
+
+
+    void deathChecker(int _hp)
+    {
+        if (_hp <= 0)
+        {
+
+            
+
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Source/LeshasScripts/Monsters/ShooterMonster.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterMonster : BasicMonster
+{
+    [SerializeField] private float _distanceToPlayer;
+
+    [SerializeField] private float _attackTrigerRadius;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        _distanceToPlayer = Vector3.Distance(_player.transform.position, transform.position);
+        
+        if (_distanceToPlayer <= _attackTrigerRadius)
+        {
+            
+        }
+        else if (_distanceToPlayer >= _attackTrigerRadius)
+        {
+            
+        }
+    }
+    void shooting()
+    {
+
+    }
+   
+}
