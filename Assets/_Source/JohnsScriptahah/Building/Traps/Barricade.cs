cd24ad94653cf2bcc9d6a48142a5d8e13a1a0d8d
anjohnsun <anjohnsun@gmail.com>
2022-12-07 13:17:13 +0300

    some bullshit and wavemanager advides

--- Assets/_Source/JohnsScriptahah/Building/Traps/Barricade.cs
+++ Assets/_Source/JohnsScriptahah/Building/Traps/Barricade.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Barricade : BasicTrap
 {
@@ -10,6 +11,9 @@ public class Barricade : BasicTrap
         transform.position = transform.position - new Vector3(0, 0.2f, 0);
         GetComponent<Collider>().isTrigger = false;
         //rebake NavMesh
+        var navMesh = GameObject.FindGameObjectWithTag("NavMeshSurface");
+        NavMeshSurface navMeshSurface = navMesh.GetComponent<NavMeshSurface>();
+        navMeshSurface.BuildNavMesh();
     }
 
     public override void OnGameStateChanged(GameState newGameState)
--- Assets/_Source/LeshasScripts/Wave/WaveController.cs
+++ Assets/_Source/LeshasScripts/Wave/WaveController.cs
@@ -12,14 +12,22 @@ public class WaveController : MonoBehaviour
     [SerializeField] private GameObject _enemyFlyPrefab;
     [SerializeField] private GameObject _enemyAgrrPrefab;
     [SerializeField] private Vector3 _enemySpawn;
-    
-    
+
+    /*
+     * private Dictionary<BasicMonster, count> EnemiesCount= new Dictionary<BasicMonster, count>;
+     * 
+     */
+
     void Start()
     {
-        SpawnWave();
+        StartCoroutine(SpawnWaveCoroutine());
+
+        /*
+         * EnemiesCount.add();//добавить все кол-ва врагов в словарь
+         */
     }
     
-    private void SpawnWave()
+    private IEnumerator SpawnWaveCoroutine()
     {
         for (int i = 0; i < _waves.Count; i++)
         {
@@ -40,5 +48,11 @@ public class WaveController : MonoBehaviour
                 Instantiate(_enemyShootPrefab, _enemySpawn, Quaternion.identity);
             }
         }
+        yield return null;
+        //yield return new WaitForSeconds(random);
+
+        /*
+         * ѕока не все значени€ в словаре == 0, ¬ыбрать рандомный тип и заспавнить, после случайной задержки N секунд повторить.
+         */
     }
 }
